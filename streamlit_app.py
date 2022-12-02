55d26f3e06ee85145c0cd153a47f6fa090a1fe66
noctetemp <53593281+noctetemp@users.noreply.github.com>
2022-12-02 19:01:05 +0900

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,35 @@
+import streamlit as st
+import pandas as pd
+import altair as alt
+import numpy as np
+
+st.header('st.write')
+
+# Example 1
+
+st.write('Hello,  *World!* :sunglasses: ')
+
+# Example 2
+st.write(1234)
+
+# Example 3
+df = pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 24, 30, 40]
+})
+
+st.write(df)
+
+# Example 4
+
+st.write('Below is a DataFrame', df, 'Above is a DataFrame')
+
+# Example 5
+df2 = pd.DataFrame(
+    np.random.randn(200, 3),
+    columns=['a', 'b', 'c']
+)
+c = alt.Chart(df2).mark_circle().encode(
+    x='a', y='b', size='c', tooltip=['a', 'b', 'c']
+)
+st.write(c)
